1c332b2e484fd3e75ac341f3684194af247a9121
=Moshiur Rahman <moshiur.amit@gmail.com>
2017-04-25 16:17:42 +0600

    todo update

---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'susy'
+require 'breakpoint'
+
+
+
+project_type = :stand_alone
+http_path = "/"
+css_dir = "app/css"
+sass_dir = "app/scss"
+images_dir = "app/images"
+javascripts_dir = "app/js"
+
+# line_comments = false
+# preferred_syntax = :sass
+relative_assets = true
+# output_style = :expanded or :nested or :compact or :compressed
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/////////////////////////////////////
+// Required 
+/////////////////////////////////////
+
+var gulp = require('gulp'),
+		uglify = require('gulp-uglify');
+
+/////////////////////////////////////
+// Script Tasks
+/////////////////////////////////////
+
+
+
+gulp.task('default',function(){
+	console.log("Hello Gulp");
+});
+
+gulp.task('scripts', function() {
+	console.log("Its Work");
+});
+
